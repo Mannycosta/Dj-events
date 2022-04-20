fd70576c40238ed85d5dafd6346fea53213f9ca3
Emmanuel <costatechsolutions@gmail.com>
2022-04-20 16:00:49 -0400

    Layout, Header, and Footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,9 @@
-export default function MainEventPage() {
+import Layout from "../../components/Layout";
+
+export default function EventPage() {
 	return (
-		<div>
-			<h1>Main event page</h1>
-		</div>
+		<Layout>
+			<h1>Event page</h1>
+		</Layout>
 	);
 }
